4ecb43c45d7c37e06167c0ecf4395ab3b8be6bae
Hicham Mokaddem <73243340+mokaddemhicham@users.noreply.github.com>
2021-05-04 22:03:37 +0000

    Update and rename script.js to script.min.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-AOS.init();
-// Toggle Navbar
-
-let toggleBtn = document.querySelector(".menu-hamburger"),
-    navBar = document.querySelector(".main-nav");
-
-toggleBtn.onclick = ()=>{
-    navBar.classList.toggle("active");
-}
